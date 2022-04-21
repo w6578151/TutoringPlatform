f22e0a52024970d920feb0a613838faddb801daf
Huang <1169960241@qq.com>
2022-04-21 11:16:39 +0800

    1.0

---
+++ src/test/java/com/example/tutoringplatform/TutoringPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutoringplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoringPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
